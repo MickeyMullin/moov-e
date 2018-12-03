2ab98d908fec28d1898e6063ed3f8b216f8be727
Mickey Mullin <wolf@mickeymullin.com>
2018-12-03 04:12:41 -0500

    Added IMDB info calls

--- src/group.js
+++ src/group.js
@@ -7,16 +7,19 @@ export default class Group extends Component {
 
     this.state = Object.assign({}, {
       showMovies: {},
+      movieCards: {},
     }, this.state)
   }
 
   componentDidMount() {
     console.log('componentDidMount, my good man')
     const showMovies = {}
+    const movieCards = {}
     this.props.groups.forEach(group => {
       showMovies[group] = this.props.expandAll
+      movieCards[group] = false
     })
-    this.setState({ showMovies })
+    this.setState({ showMovies, movieCards })
   }
 
   componentDidUpdate(prevProps) {
@@ -45,6 +48,12 @@ export default class Group extends Component {
             variant="contained"
             onClick={() => this.toggleGroup(group)}
           >{group}</Button>
+          <Button
+            id={group + 'toggle'}
+            variant="contained"
+            onClick={() => this.toggleType(group)}
+            className={this.state.showMovies[group] ? '' : 'hidden'}
+          >{group} Cards</Button>
           <div className={this.state.showMovies[group] ? '' : 'hidden'}>
             <ol>
               {this.renderMovies(group)}
@@ -61,9 +70,50 @@ export default class Group extends Component {
     const movies = []
     // TODO: title-sort
     this.props.movies[group].forEach((movie, idx) => {
-      movies.push(
-        <li key={idx}>{movie.title}</li>
-      )
+      if (!this.state.movieCards[group]) {
+        movies.push(
+          <li key={idx}>{movie.title}</li>
+        )
+      } else {
+        if (movie.imdbError) {
+          movies.push(<li key={idx}>{movie.title} <em>(info error: {movie.imdbError})</em></li>)
+        } else if (movie.lastImdbUpdate) {
+          let rtRate = 'N/A'
+          if (movie.ratings && movie.ratings.filter) {
+            const rt = movie.ratings.filter(r => r.Source === 'Rotten Tomatoes')
+            if (rt.length && rt[0].Value) {
+              rtRate = rt[0].Value
+            }
+          }
+          movies.push(
+            <div key={idx} className="card">
+              <h3>{movie.title} ({movie.year}) [{movie.rated}]</h3>
+                <img className="poster" src={movie.poster} alt={movie.title} height="200" align="right" />
+                <div>
+                  <strong>Starring: </strong>
+                  <span>{movie.actors}</span>
+                </div>
+                <div>
+                  <strong>Genres: </strong>
+                  <span>{movie.genres}</span>
+                </div>
+                <div className="rating">
+                  <span><img className="icon-rating" src="icon-imdb.png" alt="IMDB" /> {movie.rating}</span>
+
+                  &nbsp;
+
+                  <span><img className="icon-rating" src="icon-rottentomatoes.png" alt="Rotten Tomatoes" /> {rtRate}</span>
+
+                  &nbsp;
+
+                  <span><img className="icon-rating" src="icon-metacritic.png" alt="Metacritic" />  {movie.metascore}</span>
+                </div>
+              </div>
+          )
+        } else {
+          movies.push(<li key={idx}>{movie.title} <em>[...]</em></li>)
+        }
+      }
     })
     return movies
   }
@@ -72,6 +122,12 @@ export default class Group extends Component {
     const showMovies = this.state.showMovies
     showMovies[groupName] = !showMovies[groupName]
     this.setState({ showMovies })
+  }
+
+  toggleType(groupName) {
+    const movieCards = this.state.movieCards
+    movieCards[groupName] = !movieCards[groupName]
+    this.setState({ movieCards })
   }
 
   expandAllGroups(isExpanded) {
